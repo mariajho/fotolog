cf1b1a72acee057bd361d82410e9475901cca500
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-05-15 12:18:51 -0400

    comienzo-ejercicio

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
 function agregaPosteo() {
-//escribe aca el codigo
+	var containerPosteos = documentElementById("contenedor-posteos");
+	var nombreFF = documento.getElementById("nombre").value;
+	var contenidoFF = documentElementById("cajaṕosteos").value;
+	var nuevoPost = documentElementById("div");
+	var contenedorNombre =document.createElement("strong");
+	var contenedorPost = document.createElement("p");
+	var separacion = document.createElement("hr");
+	var parrafCorazon = documentElement('p');
+	var i = document.createElement('i');
+
+	parrafCorazon.appendChild(i);
+	parrafCorazon.setAttribute("class", "corazon");
+	i.setAttribute("class", "fa fa-heart");
+	i.setAttribute("arial-hidden", "true");
+
+	var nodoNombre = document.createTextNode(nombreFF + " escribio: ");
+	var nodoPosteo = document.createTextNode(contenidoFF);
+
+	contenedorNombre.appendChild(nodoNombre);
+	contenedor.Post.appendChild(nodoPosteo);
+	nuevoPost.appendChild(contenedorNombre);
+	nuevoPost.appendChild(contenedorPost);
+	nuevoPost.appendChild(parrafCorazon);
+	nuevoPost.appendChild(separacion);
+
+	i.addEventListener("click", function(){
+		i.classList.toggle('rojo');
+	});
+
+	nuevoPost.setAttribute("class", "posteo");
+	containerPosteos.appendChild(nuevoPost);
+	documentElementById("nombre").value = "";
+	documentElementById("cajaposteos").value = "";
+
+
+
 }
